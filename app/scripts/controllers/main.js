e494091b36a59c164320b61ecc39941a41a33d01
leroyfernandes <leroy.a.fernandes@gmail.com>
2014-03-08 19:45:00 -0500

    Yo Angular scaffold with corrected app name

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ccApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
